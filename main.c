62a88257f39768d641f768db9b74fce42cca27cb
Fellipe - insidemybrain <fellipe.bravo@gmail.com>
2013-11-21 10:58:24 -0200

    Reformulação do Ranking e adição de alguns comentários importantes

--- main.c
+++ main.c
@@ -7,7 +7,7 @@
 |              |  ___  | /___)(_   _)| ___ | / ___)/ _ \ | | / _  || ___ | /___)               |
 |              | |   | ||___ |  | |_ | ____|| |   | |_| || |( (_| || ____||___ |               |
 |              |_|   |_|(___/    \__)|_____)|_|    \___/ |_| \____||_____)(___/                |
-|                                                                                              |
+|                                         Projeto 2013                                         |
 |                                                                                              |
 |                     Trabalho de Laboratório de Computação 1 - IM-UFRRJ 2013                  |
 |                          Participantes                                                       |
@@ -21,23 +21,16 @@
 
 #include <stdio.h>
 #include <stdlib.h>
-#define NOME 250 //Define o máximo de caracteres possíveis para o Nick do jogador
+#define NOME_MAX 21 //Define o máximo de caracteres possíveis para o Nick do jogador
+#define PONTUACAO_MAX 999999 //Define o máximo de pontos que um jogador conseguirá obter
 
 int main(){
-	int menu = 0;
+	//int menu = 0;
 	int opcao = 0;
 	/*
 	-- Apenas deixar definido para utilização posterior
 	int i = 0;
 	while(i < 10){ i = i+1; printf("Valor de I: %d \n",i); };
-	char jogador(for i < 10; i = i+1)[NOME]// jogador1,jogador2,jogador3,jogador4,jogador5,jogador6,jogador7,jogador8,jogador9,jogador10;
-	jogador1 = "Fellipe"; jogador2 = "Fellipe"; jogador3 = "Fellipe"; jogador4 = "Fellipe";
-	jogador5 = "Fellipe"; jogador6 = "Fellipe"; jogador7 = "Fellipe"; jogador8 = "Fellipe";
-	jogador9 = "Fellipe"; jogador10 = "Fellipe";
-	int pontos1,pontos2,pontos3,pontos4,pontos5,pontos6,pontos7,pontos8,pontos9,pontos10;
-	pontos1 = "0"; pontos2 = "1"; pontos3 = "2"; pontos4 = "3";
-	pontos5 = "4"; pontos6 = "5"; pontos7 = "6"; pontos8 = "7";
-	pontos9 = "8"; pontos10 = "9";
 	int pontuacao = 0;*/
 		printf("\t-----------------------------------------------------------------------\n");
 		printf("\t|   _______                                   _      _                |\n");
@@ -46,15 +39,15 @@ int main(){
 		printf("\t|  |  ___  | /___)(_   _)| ___ | / ___)/ _ \\ | | / _  || ___ | /___)  |\n");
 		printf("\t|  | |   | ||___ |  | |_ | ____|| |   | |_| || |( (_| || ____||___ |  |\n");
 		printf("\t|  |_|   |_|(___/    \\__)|_____)|_|    \\___/ |_| \\____||_____)(___/   |\n");
-		printf("\t|                                                                     |\n");
+		printf("\t|                           Projeto 2013                              |\n");
 		printf("\t|       Trabalho de Laboratorio de Computação 1 - IM-UFRRJ 2013       |\n");
 		printf("\t|---------------------------------------------------------------------|\n");
 		printf("\t|                   Fellipe Bravo Ribeiro Pimentel                    |\n");
 		printf("\t|                      Paulo Roberto Xavier                           |\n");
 		printf("\t|                       Bianca Albuquerque                            |\n");
-		printf("\t-----------------------------------------------------------------------\n\n\n");
+		printf("\t-----------------------------------------------------------------------\n\n");
 
-		while(menu == 0) {
+		//while(menu == 0) { -- Retirado Temporariamente, pois ainda não há necessidade
 			printf("\t\t     ---------------------------------------------\n");
 			printf("\t\t     |               MENU DE JOGO                |\n");
 			printf("\t\t     ---------------------------------------------\n");
@@ -77,19 +70,44 @@ int main(){
 				printf("\t\t\t       JOGO - Ainda em criação!\n");
 				break;
 			case 2:
-				printf("\t\t     -----------------------------------------\n");
-				printf("\t\t     |            Ranking de Jogadores       |\n");
-				printf("\t\t     -----------------------------------------\n\n\n");
-				/*printf("\t\t     1º Lugar - %s   PONTOS: %u",jogador1,pontos1);
-				printf("\t\t     2º Lugar - %s   PONTOS: %u",jogador2,pontos2);
-				printf("\t\t     3º Lugar - %s   PONTOS: %u",jogador3,pontos3);
-				printf("\t\t     4º Lugar - %s   PONTOS: %u",jogador4,pontos4);
-				printf("\t\t     5º Lugar - %s   PONTOS: %u",jogador5,pontos5);
-				printf("\t\t     6º Lugar - %s   PONTOS: %u",jogador6,pontos6);
-				printf("\t\t     7º Lugar - %s   PONTOS: %u",jogador7,pontos7);
-				printf("\t\t     8º Lugar - %s   PONTOS: %u",jogador8,pontos8);
-				printf("\t\t     9º Lugar - %s   PONTOS: %u",jogador9,pontos9);
-				printf("\t\t     10º Lugar - %s   PONTOS: %u",jogador10,pontos10);*/
+				system("clear");
+				printf("\t\t   -------------------------------------------------------\n");
+				printf("\t\t   |                 Ranking de Jogadores                |\n");
+				printf("\t\t   -------------------------------------------------------\n");
+				printf("\t\t   |     COLOCAÇÃO  |    NICKNAME    |     PONTUAÇÃO     |\n");
+				printf("\t\t   -------------------------------------------------------\n");
+				/*
+				     Futuramente utilização de:
+				printf("\t\t   | Nº Lugar - %s   PONTOS: %u   |\n",jogadorN,pontosN);
+				
+					E também alguma função para verificar quantidade de caracteres em nomes e pontos,
+					completando-os com espaços, para que não seja perdida a formatação do Ranking.
+					Algo como:
+						ESPAÇOS = NICK-NOME_MAX
+						if(NICK < NOME_MAX){
+							NICK = NICK+ESPAÇOS
+						}
+				*/
+				printf("\t\t   | 1º Lugar -   NOMEDEATE21CARACTERES   PONTOS: %u |\n",PONTUACAO_MAX);
+				printf("\t\t   | --------------------------------------------------- |\n");
+				printf("\t\t   | 2º Lugar -   NOMEDEATE21CARACTERES   PONTOS: %u |\n",PONTUACAO_MAX);
+				printf("\t\t   | --------------------------------------------------- |\n");
+				printf("\t\t   | 3º Lugar -   NOMEDEATE21CARACTERES   PONTOS: %u |\n",PONTUACAO_MAX);
+				printf("\t\t   | --------------------------------------------------- |\n");
+				printf("\t\t   | 4º Lugar -   NOMEDEATE21CARACTERES   PONTOS: %u |\n",PONTUACAO_MAX);
+				printf("\t\t   | --------------------------------------------------- |\n");
+				printf("\t\t   | 5º Lugar -   NOMEDEATE21CARACTERES   PONTOS: %u |\n",PONTUACAO_MAX);
+				printf("\t\t   | --------------------------------------------------- |\n");
+				printf("\t\t   | 6º Lugar -   NOMEDEATE21CARACTERES   PONTOS: %u |\n",PONTUACAO_MAX);
+				printf("\t\t   | --------------------------------------------------- |\n");
+				printf("\t\t   | 7º Lugar -   NOMEDEATE21CARACTERES   PONTOS: %u |\n",PONTUACAO_MAX);
+				printf("\t\t   | --------------------------------------------------- |\n");
+				printf("\t\t   | 8º Lugar -   NOMEDEATE21CARACTERES   PONTOS: %u |\n",PONTUACAO_MAX);
+				printf("\t\t   | --------------------------------------------------- |\n");
+				printf("\t\t   | 9º Lugar -   NOMEDEATE21CARACTERES   PONTOS: %u |\n",PONTUACAO_MAX);
+				printf("\t\t   | --------------------------------------------------- |\n");
+				printf("\t\t   | 10º Lugar -  NOMEDEATE21CARACTERES   PONTOS: %u |\n",PONTUACAO_MAX);
+				printf("\t\t   | --------------------------------------------------- |\n");
 				break;
 
 			/*
@@ -114,7 +132,7 @@ int main(){
 				printf("\t\t\t       Opção inválida, digite uma das opções.\n");
 				break;
 			}
-		return 0;
-		}
+		//return 0;
+		//}
 return 0;	
 }
